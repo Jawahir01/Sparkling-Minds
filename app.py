568ccd9b4f87485bdcafa7dbbc734f43bde9a73b
Jawahir01 <>
2023-11-13 13:12:00 +0000

    Connect the Application woth MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo, MongoClient
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -10,10 +14,14 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def Hi():
-    return ("hello")
+@app.route("/get_courses")
+def get_courses():
+    courses = mongo.db.courses.find()
+    return render_template("courses.html", courses=courses)
 
 
 if __name__ == "__main__":
