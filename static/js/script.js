a196a3b9ca1a3f18feb843a3eb2be872b7694e20
Jawahir01 <>
2023-12-07 13:19:12 +0000

    Add hiding functionality to flash messages

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+
+ // Function to display and hide alert messages.
+    setTimeout(function () {
+      let messagesDj = document.getElementById('messages-notes');
+      if (messagesDj) {
+        messagesDj.style.display = 'none';
+      }
+    }, 2500);
