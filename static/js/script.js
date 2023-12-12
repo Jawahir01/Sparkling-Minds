de3d5d4cb19f2e4fb8d71a7e814ef29c4dc6145b
Jawahir01 <>
2023-12-12 14:53:17 +0000

    Add top header add and user dropdown icon

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,22 @@
 
- // Function to display and hide alert messages.
-    setTimeout(function () {
-      let messagesDj = document.getElementById('messages-notes');
-      if (messagesDj) {
+// Function to display and hide alert messages.
+setTimeout(function () {
+    let messagesDj = document.getElementById('messages-notes');
+    if (messagesDj) {
         messagesDj.style.display = 'none';
-      }
-    }, 2500);
+    }
+}, 2500);
+
+// check if the user has checked the confirmation checkbox before submitting the form.
+// function validateForm() {
+//     const confCheckbox = document.getElementById('confirm');
+//     if (!confCheckbox.checked) {
+//         alert('Please confirm your informations before proceeding.');
+//         return false;
+//     }
+
+//     return true;
+// }
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
